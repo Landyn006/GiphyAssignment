723b38e44bb8e3af9776b16a159991e0494ecd4c
Landyn Littlejohn <landynlittlejohn@Landyns-MacBook-Air.local>
2026-03-26 19:10:56 -0400

    last commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 console.log("script.js loaded");
 
-
+// GIPHY API KEY
 const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
 
 const button = document.querySelector("#fetch-gif-btn");
 const gifContainer = document.querySelector("#gif-container");
+const searchInput = document.querySelector("#search-input");
 
 button.addEventListener("click", async () => {
-  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+const query = searchInput.value.trim() || "dogs";
 
-  try {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+ try {
     const res = await fetch(endpoint);
     const data = await res.json();
     const images = data.data.map(gif => gif.images.original.url);
